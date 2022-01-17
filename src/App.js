c1c2ff969eca18440edea99758c30d0722aa0447
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2022-01-17 11:42:18 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Navbar from './Components/Layouts/Navbar'
+import Header from './Components/Pages/Header'
+import Products from './Components/Pages/Products'
+import About from './Components/Pages/About'
+import Contact from './Components/Pages/Contact'
+import { css } from '@emotion/react'
+import {PacmanLoader} from 'react-spinners'
 
-function App() {
+const App = () => {
+
+  const[loading, setLoading] = useState(false);
+
+  const override = css`
+  display:block;
+  border-color:red;
+  margin-top: 50%;
+  margin-left: 30%;
+  `;
+
+  useEffect(()=>{ 
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },2000);
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {
+        loading ? <PacmanLoader color={'#3d2514'} loading={loading} css={override} size={40}/>
+        :
+        <>
+          <Navbar />
+          <Header />
+          <Products />
+          <About />
+          <Contact />
+        </>
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
